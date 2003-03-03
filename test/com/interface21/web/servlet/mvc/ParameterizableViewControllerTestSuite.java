73cf0e491501a90524a708f2e6cf696df4b7609e
Rod Johnson <rjohnson@interface21.com>
2003-03-03 22:50:56 +0000

    First version

---
+++ test/com/interface21/web/servlet/mvc/ParameterizableViewControllerTestSuite.java
@@ -0,0 +1,54 @@
+
+package com.interface21.web.servlet.mvc;
+
+import javax.servlet.ServletException;
+
+import servletapi.TestHttpRequest;
+
+import com.interface21.web.servlet.ModelAndView;
+import com.interface21.web.servlet.mvc.ParameterizableViewController;
+
+import junit.framework.TestCase;
+
+/**
+ * @version $Id$
+ * @author Rod Johnson
+ * @since March 2, 2003
+ * 
+ */
+public class ParameterizableViewControllerTestSuite extends TestCase {
+
+	/**
+	 * Constructor for ParameterizableViewControllerTestSuite.
+	 * @param arg0
+	 */
+	public ParameterizableViewControllerTestSuite(String arg0) {
+		super(arg0);
+	}
+	
+	
+	public void testPropertyNotSet() throws Exception {
+		ParameterizableViewController pvc = new ParameterizableViewController();
+		try {
+			pvc.afterPropertiesSet();
+			fail("should require viewName property to be set");
+		}
+		catch (ServletException ex){
+			// ok
+		}
+	}
+	
+	public void testModelIsCorrect() throws Exception {
+		String viewName = "viewName";
+		ParameterizableViewController pvc = new ParameterizableViewController();
+		pvc.setViewName(viewName);
+		pvc.afterPropertiesSet();
+		// We don't care about the params
+		ModelAndView mv = pvc.handleRequest(new TestHttpRequest(null, "GET", "foo.html"), null);
+		assertTrue("model has no data", mv.getModel().size() == 0);
+		assertTrue("model has correct viewname", mv.getViewname().equals(viewName));
+		
+		assertTrue("getViewName matches", pvc.getViewName().equals(viewName));
+	}
+
+}
