e6e7a5f3f138818671bae7c498328ff687329d4b
Rod Johnson <rjohnson@interface21.com>
2003-03-10 07:42:29 +0000

    First version

---
+++ src/com/interface21/beans/propertyeditors/PropertyValuesEditor.java
@@ -0,0 +1,34 @@
+package com.interface21.beans.propertyeditors;
+
+import java.beans.PropertyEditorSupport;
+import java.util.Properties;
+
+import com.interface21.beans.MutablePropertyValues;
+
+/**
+ * Editor for PropertyValues objects. Not
+ * a GUI editor.
+ * <br>NB: this editor must be registered with the JavaBeans API before it
+ * will be available. Editors in this package are
+ * registered by BeanWrapperImpl.
+ * <br>The required format is defined in java.util.Properties documentation.
+ * Each property must be on a new line.
+ * <br>
+ * The present implementation relies on a PropertiesEditor.
+ * @author Rod Johnson
+ */
+public class PropertyValuesEditor extends PropertyEditorSupport {
+	
+	
+	/**
+	 * @see PropertyEditor#setAsText(String)
+	 */
+	public void setAsText(String s) throws IllegalArgumentException {
+		PropertiesEditor pe = new PropertiesEditor();
+		pe.setAsText(s);
+		Properties props = (Properties) pe.getValue();
+		setValue(new MutablePropertyValues(props));
+	}
+
+}
+
---
+++ test/com/interface21/beans/factory/DummyFactory.java
@@ -0,0 +1,69 @@
+/*
+ * The Spring Framework is published under the terms
+ * of the Apache Software License.
+ */
+ 
+package com.interface21.beans.factory;
+
+import com.interface21.beans.BeansException;
+import com.interface21.beans.TestBean;
+import com.interface21.beans.factory.support.AbstractFactoryBean;
+
+/**
+ * Simple factory to allow testing of FactoryBean 
+ * support in AbstractBeanFactory. Depending on whether its
+ * singleton property is set, it will return a singleton
+ * or a prototype instance.
+ * Implements InitializingBean interface, so we can check that
+ * factories get this lifecycle callback if they want.
+ * @author Rod Johnson
+ * @since 10-Mar-2003
+ * version $Id$
+ */
+public class DummyFactory extends AbstractFactoryBean implements InitializingBean {
+	
+	public static final String SINGLETON_NAME = "Factory singleton";
+	
+	private boolean isInitialized;
+	
+	private TestBean testBean;
+	
+	public DummyFactory() {
+		this.testBean = new TestBean();
+		this.testBean.setName(SINGLETON_NAME);
+		this.testBean.setAge(25);
+	}
+	
+	public void afterPropertiesSet() {
+		if (isInitialized)
+			throw new RuntimeException("Cannot call afterPropertiesSet twice on the one bean");
+		this.isInitialized = true;
+	}
+	
+	/**
+	 * Was this initialized by invocation of the
+	 * afterPropertiesSet() method from the InitializingBean interface?
+	 */
+	public boolean wasInitialized() {
+		return this.isInitialized;
+	}
+
+	/**
+	 * Return the managed object, supporting both singleton
+	 * and prototype mode.
+	 * @see com.interface21.beans.factory.FactoryBean#getObject()
+	 */
+	public Object getObject() throws BeansException {
+		if (isSingleton()) {
+			//System.out.println("DummyFactory returned new SINGLETON");
+			return this.testBean;
+		}
+		else {
+			//System.out.println("DummyFactory created new PROTOTYPE");
+			TestBean prototype = new TestBean("prototype created at " + System.currentTimeMillis(), 11);
+			//System.out.println("prot name is " + prototype.getName());
+			return prototype;
+		}
+	}
+
+}
