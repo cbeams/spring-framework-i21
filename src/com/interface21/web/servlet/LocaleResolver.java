6e111798cbfd9fb372a9c0f147fd9c5ea739614e
Juergen Hoeller <jhoeller@interface21.com>
2003-03-20 09:27:20 +0000

    refined JavaDoc

--- src/com/interface21/web/servlet/LocaleResolver.java
+++ src/com/interface21/web/servlet/LocaleResolver.java
@@ -6,12 +6,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Author: jho
- * Date: 27.02.2003
+ * Interface for web-based locale resolution strategies that allows for
+ * both locale resolution via the request and locale modification via
+ * request and response.
+ *
+ * This interface allows for implementations based on request, session,
+ * cookies, etc. The default implementantion is AcceptHeaderLocaleResolver,
+ * simply using the request's locale provided by the respective HTTP header.
+ *
+ * @author Juergen Hoeller
+ * @since 27.02.2003
+ * @see com.interface21.web.servlet.i18n.AcceptHeaderLocaleResolver
  */
 public interface LocaleResolver {
 
-  Locale resolveLocale(HttpServletRequest request);
+  /**
+   * Resolve the current locale via the given request.
+   * Should return a default locale as fallback in any case.
+   * @param request  the request to be used for resolution
+   * @return the current locale
+   */
+	Locale resolveLocale(HttpServletRequest request);
 
-  void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale);
+  /**
+   * Set the current locale to the given one.
+   * @param request  the request to be used for locale modification
+   * @param response  the response to be used for locale modification
+   * @param locale  the new locale
+   */
+	void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale);
 }
