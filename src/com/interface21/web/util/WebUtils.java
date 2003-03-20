241ac09b6390379838ada4af3ccc3354b64f94d3
Juergen Hoeller <jhoeller@interface21.com>
2003-03-20 09:24:55 +0000

    added support for absolute file paths to getResourceInputStream

--- src/com/interface21/web/util/WebUtils.java
+++ src/com/interface21/web/util/WebUtils.java
@@ -2,6 +2,8 @@ package com.interface21.web.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Enumeration;
@@ -18,7 +20,7 @@ import javax.servlet.http.HttpServletRequest;
  * @author  Rod Johnson
  */
 public abstract class WebUtils {
-	
+
 	/**
 	 * @return the first cookie with the given name or null if none is found
 	 * Note that multiple cookies can have the same name but different paths or domains
@@ -34,54 +36,52 @@ public abstract class WebUtils {
 		}
 		return null;
 	}
-	
-	public static boolean isRemote(String url) {
-		try {
-			URL confUrl = new URL(url);
-			return true;
-		}
-		catch (MalformedURLException ex) {
-			return false;
-		}
-	}
-	
-	/** Given a String which may be either a URL or a path within this WAR,
-	 * open an input stream allowing access to its contents.
-	 * Note that callers are responsible for closing the input stream.
-	 * @param url String which may be either a URL (e.g. http://somecompany.com/foo.xml)
+
+	/**
+	 * Given a String which may be either a URL, an absolute file path, or a path
+	 * within the WAR. Opens an input stream allowing access to its contents.
+	 * Note: Callers are responsible for closing the input stream.
+	 * @param path String which may be either a URL (e.g. http://somecompany.com/foo.xml)
 	 * or a path within this WAR (e.g. /WEB-INF/data/file.dat)
-	 * @param servletConfig the application's servlet config. We'll need this if we
+	 * @param servletContext the application's servlet context. We'll need this if we
 	 * need to load from within the WAR
 	 * @throws IOException if we can't open the resource
 	 * @return an InputStream for the resources contents.
 	 * <B>Callers are responsible for closing this input stream.</B>
 	 */
-	public static InputStream getResourceInputStream(String url, ServletContext servletContext) throws IOException {
-		//Category.getInstance(Utils.class).debug("Looking up resource stream [" + url + "]");
-		if (!isRemote(url)) {
-			// Load from within WAR
-			if (!url.startsWith("/"))
-				url = "/" + url;
-			InputStream is = servletContext.getResourceAsStream(url);
-			if (is == null)
-				throw new IOException("Can't open " + url);
-			return is;
-		}
-		else {
-			// Remote loading
-			return new URL(url).openStream();
+	public static InputStream getResourceInputStream(String path, ServletContext servletContext) throws IOException {
+		//Category.getInstance(Utils.class).debug("Looking up resource stream [" + path + "]");
+		try {
+			// try URL
+			URL url = new URL(path);
+			return url.openStream();
+
+		} catch (MalformedURLException ex) {
+			// no URL -> file path
+			File file = new File(path);
+			if (!file.isAbsolute()) {
+				// Load from within WAR
+				if (!path.startsWith("/"))
+					path = "/" + path;
+				InputStream is = servletContext.getResourceAsStream(path);
+				if (is == null)
+					throw new IOException("Can't open " + path);
+				return is;
+			} else {
+				// Remote loading
+				return new FileInputStream(path);
+			}
 		}
 	}	// getResourceInputStream
-	
-	
+
+
 	/** FOR BACKWARD COMPATIBILITY ONLY
 	 * <B>Callers are responsible for closing this input stream.</B>
 	 */
 	public static InputStream getResourceInputStream(String url, ServletConfig servletConfig) throws IOException {
 		return getResourceInputStream(url, servletConfig.getServletContext());
 	}	// getResourceInputStream
-	
-	
+
 	/** Given a servlet path string, determine the directory within the WAR
 	 * this belongs to, ending with a /. For example, /cat/dog/test.html would be
 	 * returned as /cat/dog/. /test.html would be returned as /
@@ -93,71 +93,66 @@ public abstract class WebUtils {
 		String left = servletPath.substring(0, servletPath.lastIndexOf("/") + 1);
 		return left;
 	}
-	
-	
+
+
 	/** Return the URL of the root of the current application
 	 */
 	public static String getURLToApplication(HttpServletRequest request) {
 		return request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath() + "/";
 	}
-	
-	
+
+
 	/** Convenience method to return a map from un-prefixed property names
 	 * to values. E.g. with a prefix of price, price_1, price_2 produce
 	 * a properties object with mappings for 1, 2 to the same values.
-     * @param request HTTP request in which to look for parameters
-     * @param base beginning of parameter name. If this is null or the empty string,
+	 * @param request HTTP request in which to look for parameters
+	 * @param base beginning of parameter name. If this is null or the empty string,
 	 * all parameters will match
-     * @return properties mapping request parameters <b>without the prefix</b>
-     */
-    public static Properties getParametersStartingWith(ServletRequest request, String base) {
-        Enumeration enum = request.getParameterNames();
-        Properties props = new Properties();
-		
+	 * @return properties mapping request parameters <b>without the prefix</b>
+	 */
+	public static Properties getParametersStartingWith(ServletRequest request, String base) {
+		Enumeration enum = request.getParameterNames();
+		Properties props = new Properties();
+
 		if (base == null)
 			base = "";
-        while (enum != null && enum.hasMoreElements()) {
-            String paramName = (String) enum.nextElement();
+		while (enum != null && enum.hasMoreElements()) {
+			String paramName = (String) enum.nextElement();
 			if (base == null || "".equals(base) || paramName.startsWith(base)) {
-                String val = request.getParameter(paramName);
+				String val = request.getParameter(paramName);
 				String unprefixed = paramName.substring(base.length());
 				props.setProperty(unprefixed, val);
 			}
-        }
-        return props;
-    }   // getParameterValuesStartingWith
-	
-	
-	
+		}
+		return props;
+	}   // getParameterValuesStartingWith
+
 	public static void main(String[] args) {
 		String spec = "foo.html";
 		String contexts = "http://127.0.0.1/barrier/bunny.jsp";
 		com.interface21.util.StopWatch sw = new com.interface21.util.StopWatch();
 		sw.start("1000 urls");
 		try {
-			
+
 			for (int i = 0; i < 100000; i++) {
 				java.net.URL context = new java.net.URL(contexts);
 				java.net.URL u = new java.net.URL(context, spec);
 				String s = u.toExternalForm();
-				
+
 			}
 			//System.out.println(u);
 			sw.stop();
 			System.out.println(sw);
-		}
-		catch (Exception ex) {
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 		sw.stop();
-		
-		
-		
-		
-		String [] tests = new String[] { "/dog/cat.jsp", "/index.jsp", "/dog", "/dog/", "/dog/cat/big/a065.html", "/cat/dog/" };
+
+
+		String[] tests = new String[]{"/dog/cat.jsp", "/index.jsp", "/dog", "/dog/", "/dog/cat/big/a065.html", "/cat/dog/"};
 		for (int i = 0; i < tests.length; i++) {
 			System.out.println("For " + tests[i] + " result is " + getDirectoryForServletPath(tests[i]));
 		}
 	}
-	
+
 }
