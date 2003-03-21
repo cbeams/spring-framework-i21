25c68ee87ceeefdcc337f227b681d173eb92ae18
Juergen Hoeller <jhoeller@interface21.com>
2003-03-21 14:56:45 +0000

    reworked config file loading, renamed "url" property to "path"

--- src/com/interface21/web/servlet/view/velocity/VelocityConfigurer.java
+++ src/com/interface21/web/servlet/view/velocity/VelocityConfigurer.java
@@ -27,7 +27,7 @@ import com.interface21.web.util.WebUtils;
  * JavaBean to configure Velocity, by setting the location of the Velocity properties
  * file. This bean must be included in the application context of any application using
  * Velocity with the Interface21 framework.
- * <br>The optional url property sets the location within the WAR of the Velocity properties file.
+ * <br>The optional path property sets the location within the WAR of the Velocity properties file.
  * By default it will be sought in the /WEB-INF directory, with the name velocity.properties.
  * <br>This bean exists purely to configure Velocity. It exposes no methods other than
  * initialization methods, and is not meant to be referenced by application components.
@@ -44,20 +44,20 @@ public class VelocityConfigurer extends WebApplicationObjectSupport {
 	*/
 	protected final Logger logger = Logger.getLogger(getClass().getName());
 
-	public static final String DEFAULT_VELOCITY_PROPERTIES = "velocity.properties";
+	public static final String DEFAULT_VELOCITY_PROPERTIES = "/WEB-INF/velocity.properties";
 
-	private String url = "/WEB-INF/velocity.properties";
+	private String path = DEFAULT_VELOCITY_PROPERTIES;
 
 	/**
 	 * Optionally override location of the Velocity properties file.
 	 */
-	public void setUrl(String url) {
-		this.url = url;
+	public void setPath(String path) {
+		this.path = path;
 	}
 
 	/**
 	 *  Initializes the Velocity runtime, first calling 
-	 *  loadConfiguration(ServletConvig) to get a 
+	 *  loadConfiguration(ServletConfig) to get a
 	 *  java.util.Properties of configuration information
 	 *  and then calling Velocity.init().  Override this
 	 *  to do anything to the environment before the 
@@ -67,20 +67,20 @@ public class VelocityConfigurer extends WebApplicationObjectSupport {
 	protected void init() throws ApplicationContextException {
 		try {
 			Properties p = new Properties();
-			logger.info("Loading Velocity properties from [" + this.url + "]");
-			p.load(WebUtils.getResourceInputStream(this.url, getWebApplicationContext().getServletContext()));
+			logger.info("Loading Velocity properties from [" + this.path + "]");
+			p.load(getWebApplicationContext().getResourceAsStream(this.path));
 
 			Velocity.init(p);
 		}
 		catch (ServletException e) {
-			throw new ApplicationContextException("Error loading Velocity config from [" + this.url + "]", e);
+			throw new ApplicationContextException("Error loading Velocity config from [" + this.path + "]", e);
 		}
 		catch (IOException e) {
-			throw new ApplicationContextException("Error loading Velocity config from [" + this.url + "]", e);
+			throw new ApplicationContextException("Error loading Velocity config from [" + this.path + "]", e);
 		}
 		catch (Exception e) {
 			throw new ApplicationContextException(
-				"Error initializing Velocity from properties file (loaded OK) @[" + this.url + "]",
+				"Error initializing Velocity from properties file (loaded OK) @[" + this.path + "]",
 				e);
 		}
 	}
