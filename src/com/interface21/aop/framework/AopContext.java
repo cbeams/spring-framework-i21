b307de172c20ce14b6a64da91647f1ce9073e434
Rod Johnson <rjohnson@interface21.com>
2003-04-08 20:30:14 +0000

    Javadoced

--- src/com/interface21/aop/framework/AopContext.java
+++ src/com/interface21/aop/framework/AopContext.java
@@ -9,10 +9,21 @@ import org.aopalliance.AspectException;
 import org.aopalliance.MethodInvocation;
 
 /**
- * 
+ * Class containing static methods used to obtain
+ * information about the current AOP invocation.
+ * The currentInvocation() method--the only public method in
+ * this class--is usable only if the AOP framework is configured
+ * to expose invocations. The framework does not expose invocation contexts
+ * by default, as there is a performance cost in doing so.
+ * <br>
+ * The functionality in this class might be used by a target
+ * object that needed access to resources on the invocation.
+ * However, this approach should not be used when there is
+ * a reasonable alternative, as it makes application code dependent on
+ * usage under AOP and--specifically--the Spring AOP framework.
  * @author Rod Johnson
  * @since 13-Mar-2003
- * @version $Revision$
+ * @version $Id$
  */
 public abstract class AopContext {
 	
@@ -23,10 +34,28 @@ public abstract class AopContext {
 	 */
 	private static ThreadLocal currentInvocation = new ThreadLocal();
 
+	/**
+	 * Internal method that the AOP framework uses to set the current
+	 * AOP context if it is configured to expose call contexts.
+	 * @param invocation the current AOP invocation context
+	 */
 	static void setCurrentInvocation(MethodInvocation invocation) {
 		currentInvocation.set(invocation);
 	}
 
+	/**
+	 * Try to return the current AOP invocation. This method
+	 * is usable only if the calling method has been invoked
+	 * via AOP, and the AOP framework has been set to
+	 * expose invocations. Otherwise, this method will throw
+	 * AspectException.
+	 * @return MethodInvocation the current AOP invocation.
+	 * Never returns null.
+	 * @throws AspectException if the invocation cannot be found,
+	 * because the method was invoked outside an AOP invocation
+	 * context or because the AOP framework has not been configured
+	 * to expose the invocation context.
+	 */
 	public static MethodInvocation currentInvocation() {
 		if (currentInvocation == null || currentInvocation.get() == null)
 			throw new AspectException("Cannot find invocation: set 'exposeInvocation' property on AopProxy to make it available");
