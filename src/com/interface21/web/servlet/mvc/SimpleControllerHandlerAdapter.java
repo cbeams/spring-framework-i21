10099234c83142fd9b19e7b25a2d4675b135aef7
Juergen Hoeller <jhoeller@interface21.com>
2003-04-08 17:25:57 +0000

    minor polishing

--- src/com/interface21/web/servlet/mvc/SimpleControllerHandlerAdapter.java
+++ src/com/interface21/web/servlet/mvc/SimpleControllerHandlerAdapter.java
@@ -23,49 +23,40 @@ import com.interface21.web.servlet.ModelAndView;
  */
 public class SimpleControllerHandlerAdapter implements HandlerAdapter {
 	
-	/** Application context */
-	private ApplicationContext ctx;
+	private ApplicationContext applicationContext;
 
-	/** Creates new RequestHandlerAdapter */
-    public SimpleControllerHandlerAdapter() {
-    }
+	public SimpleControllerHandlerAdapter() {
+	}
 
-	/** Set the ApplicationContext object used by this object
+	/**
+	 * Set the ApplicationContext object used by this object
 	 * @param ctx ApplicationContext object used by this object
 	 * @throws ApplicationContextException if initialization attempted by this object
 	 * after it has access to the WebApplicatinContext fails
 	 */
 	public void setApplicationContext(ApplicationContext ctx) throws ApplicationContextException {
-		this.ctx = ctx;
+		this.applicationContext = ctx;
 	}
 	
-	/** Return the ApplicationContext used by this object.
+	/**
+	 * Return the ApplicationContext used by this object.
 	 * @return the ApplicationContext used by this object.
 	 * Returns null if setApplicationContext() has not yet been called:
 	 * this can be useful to check whether or not the object has been initialized.
 	 */
 	public ApplicationContext getApplicationContext() {
-		return ctx;
+		return applicationContext;
 	}
 	
-	/**
-	 * @see HandlerAdapter#supports(Object)
-	 */
 	public boolean supports(Object handler) {
 		return handler != null && Controller.class.isAssignableFrom(handler.getClass());
 	}
 	
-	/**
-	 * @see HandlerAdapter#handle(HttpServletRequest, HttpServletResponse, Object)
-	 */
 	public ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object delegate) throws IOException, ServletException {
 		Controller controller = (Controller) delegate;
 		return controller.handleRequest(request, response);
 	}
 	
-	/**
-	 * @see HandlerAdapter#getLastModified(HttpServletRequest, Object)
-	 */
 	public long getLastModified(HttpServletRequest request, Object delegate) {
 		if (delegate instanceof LastModified) {
 			return ((LastModified) delegate).getLastModified(request);
