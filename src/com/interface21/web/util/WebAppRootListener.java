717f2823bc71543de935218a43fd68a0f57e7ba8
Juergen Hoeller <jhoeller@interface21.com>
2003-04-23 16:01:48 +0000

    polished docs

--- src/com/interface21/web/util/WebAppRootListener.java
+++ src/com/interface21/web/util/WebAppRootListener.java
@@ -7,15 +7,16 @@ import com.interface21.web.util.WebUtils;
 
 /**
  * Listener that sets a system property to the web application root
- * directory. The key of the system property can be defined with the
- * "webAppRootKey" init parameter at the servlet context level.
+ * directory. The key of the system property can be defined with
+ * the "webAppRootKey" init parameter at the servlet context level
+ * (i.e. web.xml), the default key is "webapp.root".
  *
  * <p>Can be used for toolkits that support substition with
  * system properties (i.e. System.getProperty values),
  * like Log4J's ${key} syntax within log file locations.
  *
- * <p>Note: This listener should be placed before ContextLoaderListener
- * resp. ContextLoaderServlet in web.xml, when used for Log4J.
+ * <p>Note: This listener should be placed before
+ * ContextLoaderListener in web.xml, when used for Log4J.
  * 
  * <p>Note: Log4jConfigServlet sets the system property implicitly,
  * so there's no need for this listener in addition to it.
