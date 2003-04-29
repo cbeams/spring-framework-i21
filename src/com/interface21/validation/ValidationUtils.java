347aa6aec58270d6c5ccff5253b192be95a8c134
Juergen Hoeller <jhoeller@interface21.com>
2003-04-29 16:19:26 +0000

    added validateEmailAddress implementation

--- src/com/interface21/validation/ValidationUtils.java
+++ src/com/interface21/validation/ValidationUtils.java
@@ -1,15 +1,37 @@
-
 package com.interface21.validation;
 
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.AddressException;
+
 /**
- * This class is not implemented yet, but will contain
- * useful generic validation routines, such as a standard
- * way of validating emails.
+ * This class contains useful generic validation routines,
+ * such as a standard way of validating emails.
+ *
+ * <p>These routines can be leveraged in custom Validator implementations,
+ * rejecting respective fields via the Errors interface accordingly.
+ *
+ * @author Juergen Hoeller
+ * @see Errors
  */
 public abstract class ValidationUtils {
-	
-	public static boolean validateEmail(String email, Errors errors, String fieldName) {
-		throw new UnsupportedOperationException("validateEmail");
+
+	/**
+	 * Validate the given email address syntactically.
+	 * <p>Note: Does not check if the email address actually exists.
+	 * @param emailAddress the email address to validate
+	 * @return if the address is syntactically valid
+	 */
+	public static boolean validateEmailAddress(String emailAddress) {
+		try {
+			InternetAddress address = new InternetAddress(emailAddress);
+			address.validate();
+			// parsing succeeded -> valid
+			return true;
+		}
+		catch (AddressException ex) {
+			// parsing error -> invalid
+			return false;
+		}
 	}
 
 }
