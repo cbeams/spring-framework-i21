80cba4001191ef30487d81ced87227d7a95c731d
Luke Taylor <luke_t@users.sourceforge.net>
2003-05-01 15:28:14 +0000

    removed incorrect cutnpasted javadoc comments referring to ReflectionException

--- src/com/interface21/beans/PropertyAccessException.java
+++ src/com/interface21/beans/PropertyAccessException.java
@@ -10,35 +10,26 @@ import java.beans.PropertyChangeEvent;
  * @version $Revision$
  */
 public abstract class PropertyAccessException extends BeansException {
-	
+
 	private PropertyChangeEvent propertyChangeEvent;
-	
-	/**
-	 * Constructs an <code>ReflectionException</code> with the specified detail message.
-	 * @param msg the detail message.
-	 */
+
 	public PropertyAccessException(String mesg, PropertyChangeEvent propertyChangeEvent) {
 		super(mesg);
 		this.propertyChangeEvent = propertyChangeEvent;
 	}
-	
-	/**
-	 * Constructs an <code>ReflectionException</code> with the specified detail message.
-	 * @param msg the detail message.
-	 */
+
 	public PropertyAccessException(String mesg, Throwable t, PropertyChangeEvent propertyChangeEvent) {
 		super(mesg, t);
 		this.propertyChangeEvent = propertyChangeEvent;
 	}
-	
-	
+
 	/**
 	 * @return the PropertyChangeEvent that resulted in the problem
 	 */
 	public PropertyChangeEvent getPropertyChangeEvent() {
 		return propertyChangeEvent;
 	}
-	
+
 }
 
 
--- src/com/interface21/beans/TypeMismatchException.java
+++ src/com/interface21/beans/TypeMismatchException.java
@@ -9,19 +9,15 @@ import java.beans.PropertyChangeEvent;
  * @version $Revision$
  */
 public class TypeMismatchException extends PropertyAccessException {
-	
-	/**
-	 * Constructs an <code>ReflectionException</code> with the specified detail message.
-	 * @param msg the detail message.
-	 */
+
 	public TypeMismatchException(PropertyChangeEvent propertyChangeEvent, Class requiredType) {
 		super("Failed to convert property value of type [" + propertyChangeEvent.getNewValue().getClass().getName() + "] to required type [" + requiredType.getName() + "]", propertyChangeEvent);
 	}
-	
+
 	public TypeMismatchException(PropertyChangeEvent propertyChangeEvent, Class requiredType, Throwable t) {
 		super("Failed to convert property value of type [" + propertyChangeEvent.getNewValue().getClass().getName() + "] to required type [" + requiredType.getName() + "]", t, propertyChangeEvent);
 	}
-	
+
 }
 
 
