d95c4d301dca11953151592260b5b47fda090c8e
Rod Johnson <rjohnson@interface21.com>
2003-05-01 22:13:04 +0000

    Handles comments. In Orion 2.0 don't seem to need workaround.

--- src/com/interface21/beans/propertyeditors/PropertiesEditor.java
+++ src/com/interface21/beans/propertyeditors/PropertiesEditor.java
@@ -1,6 +1,8 @@
 package com.interface21.beans.propertyeditors;
 
 import java.beans.PropertyEditorSupport;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.Properties;
 import java.util.StringTokenizer;
 
@@ -15,6 +17,7 @@ import java.util.StringTokenizer;
  * <br>The required format is defined in java.util.Properties documentation.
  * Each property must be on a new line.
  * @author Rod Johnson
+ * @version $Id$
  */
 public class PropertiesEditor extends PropertyEditorSupport {
 	
@@ -22,13 +25,26 @@ public class PropertiesEditor extends PropertyEditorSupport {
 	 * @see java.beans.PropertyEditor#setAsText(String)
 	 */
 	public void setAsText(String s) throws IllegalArgumentException {
-		Properties props = new Properties();
 		
 		if (s == null)
 			throw new IllegalArgumentException("Cannot set properties to null");
-
-		//System.out.println("String is [" + s + "]");
-
+	
+		Properties props =
+			load(s); 
+			//parse(s);
+		setValue(props);
+	}
+	
+	
+	/**
+	 * Parse the string ourselves.
+	 * Workaround for Orion 1.6 issue
+	 * @param s
+	 * @return Properties
+	 */
+	private Properties parse(String s) {
+		Properties props = new Properties();
+		
 		// Zap whitespace
 		StringTokenizer st = new StringTokenizer(s);
 		while (st.hasMoreTokens()) {
@@ -38,6 +54,8 @@ public class PropertiesEditor extends PropertyEditorSupport {
 			// Tokens look like "/welcome.html=mainController"
 			int eqpos = tok.indexOf("=");
 			if (eqpos == -1) {
+				// We only have the property name, but
+				// the value is the the empty string
 				props.put(tok, "");
 			}
 			else {
@@ -45,21 +63,26 @@ public class PropertiesEditor extends PropertyEditorSupport {
 				String value = tok.substring(eqpos + 1);
 				props.put(key, value);
 			}
-			
+		}
+		
+		return props;
+	}	// parse
+
+
+	/** NB: the following code, using properties default works in JBoss 3.0.0,
+	 * but not Orion 1.6
+	*/
+	private Properties load(String s) {
+		Properties props = new Properties();
+		try {
+			props.load(new ByteArrayInputStream(s.getBytes()));
+		}
+		catch (IOException ex) {
+			// Shouldn't happen
+			throw new IllegalArgumentException("Failed to read String");
 		}
 
-
-		// NB: the following code, using properties default works in JBoss,
-		// but not Orion
-//		try {
-//			props.load(new ByteArrayInputStream(s.getBytes()));
-//		}
-//		catch (IOException ex) {
-//			// Shouldn't happen
-//			throw new IllegalArgumentException("Failed to read String");
-//		}
-
-		setValue(props);
+		return props;
 	}
 
 }
