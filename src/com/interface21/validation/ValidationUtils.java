f83317cfcfce15402a889dfe4b627681b04f19cd
Juergen Hoeller <jhoeller@interface21.com>
2003-05-06 15:10:14 +0000

    reactivated BindUtils, introduced ValidationUtils.invokeValidator

--- src/com/interface21/validation/ValidationUtils.java
+++ src/com/interface21/validation/ValidationUtils.java
@@ -1,19 +1,43 @@
 package com.interface21.validation;
 
-import javax.mail.internet.InternetAddress;
 import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+import org.apache.log4j.Logger;
 
 /**
- * This class contains useful generic validation routines,
- * such as a standard way of validating emails.
+ * This class offers a convenient validate method for invoking a validator,
+ * and useful generic validation routines, like for validating emails.
  *
- * <p>These routines can be leveraged in custom Validator implementations,
+ * <p>The validation routines can be leveraged in custom Validator implementations,
  * rejecting respective fields via the Errors interface accordingly.
  *
  * @author Juergen Hoeller
+ * @see Validator
  * @see Errors
  */
 public abstract class ValidationUtils {
+
+	private static Logger logger = Logger.getLogger(ValidationUtils.class);
+
+	/**
+	 * Invoke the given validator for the given object and Errors instance.
+	 * @param validator validator to be invoked, or null if no validation
+	 * @param object object to bind the parameters to
+	 * @param errors Errors instance that should store the errors
+	 */
+	public static void invokeValidator(Validator validator, Object object, Errors errors) {
+		if (validator != null) {
+			logger.debug("Invoking validator [" + validator + "]");
+			if (!validator.supports(object.getClass()))
+				throw new IllegalArgumentException("Validator " + validator.getClass() + " does not support " + object.getClass());
+			validator.validate(object, errors);
+			if (errors.hasErrors())
+				logger.debug("Validator found " + errors.getErrorCount() + " errors");
+			else
+				logger.debug("Validator found no errors");
+		}
+	}
 
 	/**
 	 * Validate the given email address syntactically.
---
+++ src/com/interface21/web/bind/BindUtils.java
@@ -0,0 +1,45 @@
+package com.interface21.web.bind;
+
+import javax.servlet.ServletRequest;
+
+import com.interface21.validation.ValidationUtils;
+import com.interface21.validation.Validator;
+
+/**
+ * Offers convenience methods for binding servlet request parameters
+ * to objects, including optional validation.
+ *
+ * @author Juergen Hoeller
+ * @since 10.03.2003
+ */
+public abstract class BindUtils {
+
+	/**
+	 * Bind the parameters from the given request to the given object.
+	 * @param request request containing the parameters
+	 * @param object object to bind the parameters to
+	 * @param objectName name of the bind object
+	 * @return the binder used (can be treated as DataBinder or Errors instance)
+	 */
+	public static ServletRequestDataBinder bind(ServletRequest request, Object object, String objectName) {
+		ServletRequestDataBinder binder = new ServletRequestDataBinder(object, objectName);
+		binder.bind(request);
+		return binder;
+	}
+
+	/**
+	 * Bind the parameters from the given request to the given object,
+	 * invoking the given validator.
+	 * @param request request containing the parameters
+	 * @param object object to bind the parameters to
+	 * @param objectName name of the bind object
+	 * @param validator validator to be invoked, or null if no validation
+	 * @return the binder used (can be treated as Errors instance)
+	 */
+	public static ServletRequestDataBinder bindAndValidate(ServletRequest request, Object object, String objectName, Validator validator) {
+		ServletRequestDataBinder binder = bind(request, object, objectName);
+		ValidationUtils.invokeValidator(validator, object, binder);
+		return binder;
+	}
+
+}
