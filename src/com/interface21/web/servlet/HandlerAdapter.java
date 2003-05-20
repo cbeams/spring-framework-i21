1e09e9d26451b9d6fb4ed6ecfb3fd2654efb98ab
Juergen Hoeller <jhoeller@interface21.com>
2003-05-20 17:45:19 +0000

    polishing and docs

--- src/com/interface21/web/servlet/HandlerAdapter.java
+++ src/com/interface21/web/servlet/HandlerAdapter.java
@@ -8,39 +8,35 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.interface21.context.ApplicationContextAware;
 
-
 /**
- * <b>MVC framework SPI interface, allowing parameterization of
- * core MVC workflow.</b>
- * Interface that must be implemented for each handler type to
- * use the handler to handle a request.
- * This interface is used to allow the ControllerServlet to be indefinitely
- * extensible. The ControllerServlet accesses all installed handlers
- * through this interface, meaning that it does not contain code specific
- * to any handler type.
- * <br/>Note that a handler can be of type Object. This is
- * to enable handlers from other frameworks to be integrated
- * with this framework without custom coding.
- * <br/>This interface is not intended for application developers.</b>
- * It is available to handlers who want to develop their own web workflow.
- * Need one of these for each fancy handler type.
+ * MVC framework SPI interface, allowing parameterization of core MVC workflow.
  *
- * <p>Note: Implementations can implement the Ordered interface to be
- * able to specify a sorting order and thus a priority for getting
- * applied by ControllerServlet. Non-Ordered instances get treated as
- * lowest priority.
+ * <p>Interface that must be implemented for each handler type to handle a request.
+ * This interface is used to allow the ControllerServlet to be indefinitely
+ * extensible. The ControllerServlet accesses all installed handlers through this
+ * interface, meaning that it does not contain code specific to any handler type.
+ *
+ * <p>Note that a handler can be of type Object. This is to enable handlers from
+ * other frameworks to be integrated with this framework without custom coding.
+ *
+ * <p>This interface is not intended for application developers. It is available
+ * to handlers who want to develop their own web workflow.
+ *
+ * <p>Note: Implementations can implement the Ordered interface to be able to
+ * specify a sorting order and thus a priority for getting applied by
+ * ControllerServlet. Non-Ordered instances get treated as lowest priority.
  *
  * @author Rod Johnson
  */
 public interface HandlerAdapter extends ApplicationContextAware {
 	
-	/** Given a handler instance, return whether or not this
-	 * HandlerAdapter can support it. Usually HandlerAdapters will
-	 * base the decision on the handler type. HandlerAdapters
-	 * will normally support only one handler type.
-	 * <br/>A typical implementation:
+	/**
+	 * Given a handler instance, return whether or not this* HandlerAdapter can
+	 * support it. Usually HandlerAdapters will base the decision on the handler
+	 * type. HandlerAdapters will normally support only one handler type.
+	 * <p>A typical implementation:
 	 * <code>
-	 * return handler != null && RequestHandler.class.isAssignableFrom(handler.getClass());
+	 * return handler != null && MyHandler.class.isAssignableFrom(handler.getClass());
 	 * </code>
 	 * @param handler handler object to check
 	 * @return whether or not this object can use the given handler
@@ -48,26 +44,24 @@ public interface HandlerAdapter extends ApplicationContextAware {
 	boolean supports(Object handler); 
 	
 	/**
-	 * Same contract as for Servlet.getLastModified
-	 * Can simply return -1 if there's no support in the 
-	 * delegate class
+	 * Same contract as for Servlet.getLastModified.
+	 * Can simply return -1 if there's no support in the delegate class.
 	 */
 	long getLastModified(HttpServletRequest request, Object delegate);
 	
 	/**
 	 * Use the given handler to handle this request.
 	 * The workflow that is required may vary widely.
-	 * @param delegate handler to use. This object must have
-	 * previously been passed to the supports() method of this interface,
-	 * which must have returned true.
-	 * @throws IOException this exception will not normally be thrown.
+	 * @param delegate handler to use. This object must have previously been passed
+	 * to the supports() method of this interface, which must have returned true.
 	 * Implementations that generate output themselves (and return null
-	 * from this method) may encounter and need not catch IOExceptions.
+	 * from this method) may encounter IOExceptions.
+	 * @throws IOException in case of I/O errors
 	 * @throws ServletException if there is an error
 	 * @return ModelAndView object with the name of the view and the required
-	 * model data. Return null if the request was been handled directly.
-	 * DO WE WANT TO ALLOW THIS
+	 * model data, or null if the request has been handled directly.
 	 */
-	ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object delegate) throws IOException, ServletException;
+	ModelAndView handle(HttpServletRequest request, HttpServletResponse response, Object delegate)
+	    throws IOException, ServletException;
 
-}	// interface HandlerAdapter
+}
--- src/com/interface21/web/servlet/HandlerMapping.java
+++ src/com/interface21/web/servlet/HandlerMapping.java
@@ -15,27 +15,28 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
 import com.interface21.context.ApplicationContextAware;
-import com.interface21.context.ApplicationContextException;
 
 /**
  * Interface to be implemented by objects that define a mapping between
- * requests and handler objects.  
- * <br/>This class can be implemented by application developers,
- * although this is not necessary, as the default URLHandlerMapping class
- * is available if no custom handlers are registered.
- * <br/>The ability to parameterize this mapping is a powerful and
- * unusual capability of this MVC framework. For example, it is
- * possible to write a custom mapping based on session state, cookie
- * state or many other variables. No other MVC framework I am aware
- * of is equally flexible.
+ * requests and handler objects.
  *
- * <p>Note: Implementations can implement the Ordered interface to be
- * able to specify a sorting order and thus a priority for getting
- * applied by ControllerServlet. Non-Ordered instances get treated as
- * lowest priority.
+ * <p>This class can be implemented by application developers,
+ * although this is not necessary, as the default BeanNameUrlHandlerMapping
+ * is available if no custom handlers are registered.
+ *
+ * <p>The ability to parameterize this mapping is a powerful and unusual
+ * capability of this MVC framework. For example, it is possible to write
+ * a custom mapping based on session state, cookie state or many other
+ * variables. No other MVC framework seems to be equally flexible.
+ *
+ * <p>Note: Implementations can implement the Ordered interface to be able to
+ * specify a sorting order and thus a priority for getting applied by
+ * ControllerServlet. Non-Ordered instances get treated as lowest priority.
  *
  * @author Rod Johnson
  * @see com.interface21.core.Ordered
+ * @see com.interface21.web.servlet.handler.BeanNameUrlHandlerMapping
+ * @see com.interface21.web.servlet.handler.SimpleUrlHandlerMapping
  */
 public interface HandlerMapping extends ApplicationContextAware, LocaleResolverAware {
 	
@@ -44,9 +45,9 @@ public interface HandlerMapping extends ApplicationContextAware, LocaleResolverA
 	 * Handler objects are normally taken from the BeanFactory.
 	 * This means that implementing classes should never need to open
 	 * configuration files for themselves.
-	 * @throws Exception if this handler mapping cannot initialize correctly.
+	 * @throws ServletException if this handler mapping cannot initialize correctly.
 	 */
-	void initHandlerMapping() throws Exception;
+	void initHandlerMapping() throws ServletException;
 	
 	/**
 	 * Return a handler for this request.
@@ -63,5 +64,5 @@ public interface HandlerMapping extends ApplicationContextAware, LocaleResolverA
 	 */
 	Object getHandler(HttpServletRequest request) throws ServletException;
 
-}	// interface HandlerMapping
+}
 
