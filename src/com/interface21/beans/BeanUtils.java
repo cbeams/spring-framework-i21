117b2e317928640a45d9e2903e339828c10a16bc
Juergen Hoeller <jhoeller@interface21.com>
2003-05-20 16:21:54 +0000

    introduced copyProperties and sortByProperty

--- src/com/interface21/beans/BeanUtils.java
+++ src/com/interface21/beans/BeanUtils.java
@@ -1,9 +1,15 @@
-
 package com.interface21.beans;
+
+import java.beans.PropertyDescriptor;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Static convenience methods for JavaBeans.
+ * Provides e.g. methods for sorting lists of beans by any property.
  * @author Rod Johnson
+ * @author Juergen Hoeller
  */
 public abstract class BeanUtils {
 
@@ -24,6 +30,79 @@ public abstract class BeanUtils {
 		catch (IllegalAccessException ex) {
 			throw new FatalBeanException("Cannot instantiate [" + clazz + "]; has class definition changed? Is there a public constructor?", ex);
 		}
+	}
+
+	/**
+	 * Copy the property values of the given source bean into the given target bean,
+	 * ignoring the given ignoreProperties.
+	 * @param source source bean
+	 * @param target target bean
+	 * @throws IllegalArgumentException if the classes of source and target do not match
+	 */
+	public static void copyProperties(Object source, Object target)
+	    throws IllegalArgumentException, BeansException {
+		copyProperties(source, target, null);
+	}
+
+	/**
+	 * Copy the property values of the given source bean into the given target bean,
+	 * ignoring the given ignoreProperties.
+	 * @param source source bean
+	 * @param target target bean
+	 * @param ignoreProperties array of property names to ignore
+	 * @throws IllegalArgumentException if the classes of source and target do not match
+	 */
+	public static void copyProperties(Object source, Object target, String[] ignoreProperties)
+	    throws IllegalArgumentException, BeansException {
+		if (source == null || target == null || !source.getClass().isInstance(target))
+			throw new IllegalArgumentException("Target must an instance of source");
+		List ignoreList = (ignoreProperties != null) ? Arrays.asList(ignoreProperties) : null;
+		BeanWrapper sourceBw = new BeanWrapperImpl(source);
+		BeanWrapper targetBw = new BeanWrapperImpl(target);
+		MutablePropertyValues values = new MutablePropertyValues();
+		for (int i = 0; i < sourceBw.getPropertyDescriptors().length; i++) {
+			PropertyDescriptor sourceDesc = sourceBw.getPropertyDescriptors()[i];
+			String name = sourceDesc.getName();
+			PropertyDescriptor targetDesc = targetBw.getPropertyDescriptor(name);
+			if (targetDesc.getWriteMethod() != null &&
+			    (ignoreProperties == null || (!ignoreList.contains(name)))) {
+				values.addPropertyValue(new PropertyValue(name, sourceBw.getPropertyValue(name)));
+			}
+		}
+		targetBw.setPropertyValues(values);
+	}
+
+	/**
+	 * Sorts the given List by the given propertyName.
+	 * <p>Note: Contained objects have to provide the given propertyName in the form of a
+	 * bean property, i.e. a getXXX method.
+	 * @param source the input List
+	 * @param propertyName the property to sort by
+	 * @param ignoreCase ignore upper and lower case in Strings
+	 * @param ascending ascending or descending?
+	 * @throws IllegalArgumentException in case of a missing propertyName
+	 */
+	public static void sortByProperty(List source, String propertyName, boolean ascending, boolean ignoreCase)
+	    throws BeansException {
+		Collections.sort(source, new BeanPropertyComparator(propertyName, ascending, ignoreCase));
+	}
+
+	/**
+	 * Sorts the given source by the given bean property.
+	 * <p>Note: Contained objects have to provide the given propertyName in the form of a
+	 * bean property, i.e. a getXXX method.
+	 * @param source input source
+	 * @param propertyName the property to sort by
+	 * @param ignoreCase ignore upper and lower case in Strings
+	 * @param ascending ascending or descending?
+	 * @return the sorted source
+	 * @throws IllegalArgumentException in case of a missing propertyName
+	 */
+	public static Object[] sortByProperty(Object[] source, String propertyName, boolean ascending, boolean ignoreCase)
+	    throws BeansException {
+		Object[] result = (Object[]) source.clone();
+		Arrays.sort(result, new BeanPropertyComparator(propertyName, ascending, ignoreCase));
+		return result;
 	}
 
 }
---
+++ test/com/interface21/beans/BeanUtilsTests.java
@@ -0,0 +1,155 @@
+package com.interface21.beans;
+
+import java.beans.PropertyVetoException;
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import com.interface21.beans.propertyeditors.CustomDateEditor;
+
+/**
+ * @author Juergen Hoeller
+ * @since 19.05.2003
+ */
+public class BeanUtilsTests extends TestCase {
+
+	public BeanUtilsTests(String msg) {
+		super(msg);
+	}
+
+	public void testInstantiateClass() {
+		// give proper class
+		BeanUtils.instantiateClass(ArrayList.class);
+
+		try {
+			// give interface
+			BeanUtils.instantiateClass(List.class);
+			fail("Should have thrown FatalBeanException");
+		}
+		catch (FatalBeanException ex) {
+			// expected
+		}
+
+		try {
+			// give class without default constructor
+			BeanUtils.instantiateClass(CustomDateEditor.class);
+			fail("Should have thrown FatalBeanException");
+		}
+		catch (FatalBeanException ex) {
+			// expected
+		}
+	}
+
+	public void testCopyProperties() throws Exception {
+		TestBean tb = new TestBean();
+		tb.setName("rod");
+		tb.setAge(32);
+		tb.setTouchy("touchy");
+		TestBean tb2 = new TestBean();
+		assertTrue("Name empty", tb2.getName() == null);
+		assertTrue("Age empty", tb2.getAge() == 0);
+		assertTrue("Touchy empty", tb2.getTouchy() == null);
+
+		try {
+			BeanUtils.copyProperties(tb, "");
+			fail("Should have thrown IllegalArgumentException");
+		}
+		catch (IllegalArgumentException ex) {
+			// expected
+		}
+
+		BeanUtils.copyProperties(tb, tb2);
+		assertTrue("Name copied", tb2.getName().equals(tb.getName()));
+		assertTrue("Age copied", tb2.getAge() == tb.getAge());
+		assertTrue("Touchy copied", tb2.getTouchy().equals(tb.getTouchy()));
+	}
+
+	public void testCopyPropertiesWithIgnore() throws IllegalAccessException, PropertyVetoException {
+		TestBean tb = new TestBean();
+		tb.setName("rod");
+		tb.setAge(32);
+		TestBean tb2 = new TestBean();
+		assertTrue("Name empty", tb2.getName() == null);
+		assertTrue("Age empty", tb2.getAge() == 0);
+		assertTrue("Touchy empty", tb2.getTouchy() == null);
+
+		BeanUtils.copyProperties(tb, tb2, new String[] {"spouse", "touchy", "age"});
+		assertTrue("Name copied", tb2.getName().equals(tb.getName()));
+		assertTrue("Age still empty", tb2.getAge() == 0);
+		assertTrue("Touchy still empty", tb2.getTouchy() == null);
+	}
+
+	public void testSortByPropertyWithList() {
+		TestBean tb1 = new TestBean();
+		tb1.setName("eva");
+		tb1.setAge(25);
+		TestBean tb2 = new TestBean();
+		tb2.setName("juergen");
+		tb2.setAge(99);
+		TestBean tb3 = new TestBean();
+		tb3.setName("Rod");
+		tb3.setAge(32);
+		List tbs = new ArrayList();
+		tbs.add(tb1);
+		tbs.add(tb2);
+		tbs.add(tb3);
+
+		BeanUtils.sortByProperty(tbs, "name", true, false);
+		assertTrue("Correct 1. entry", tbs.get(0) == tb3);
+		assertTrue("Correct 2. entry", tbs.get(1) == tb1);
+		assertTrue("Correct 3. entry", tbs.get(2) == tb2);
+
+		BeanUtils.sortByProperty(tbs, "name", false, false);
+		assertTrue("Correct 1. entry", tbs.get(0) == tb2);
+		assertTrue("Correct 2. entry", tbs.get(1) == tb1);
+		assertTrue("Correct 3. entry", tbs.get(2) == tb3);
+
+		BeanUtils.sortByProperty(tbs, "name", true, true);
+		assertTrue("Correct 1. entry", tbs.get(0) == tb1);
+		assertTrue("Correct 2. entry", tbs.get(1) == tb2);
+		assertTrue("Correct 3. entry", tbs.get(2) == tb3);
+
+		BeanUtils.sortByProperty(tbs, "age", false, false);
+		assertTrue("Correct 1. entry", tbs.get(0) == tb2);
+		assertTrue("Correct 2. entry", tbs.get(1) == tb3);
+		assertTrue("Correct 3. entry", tbs.get(2) == tb1);
+	}
+
+	public void testSortByPropertyWithArray() {
+		TestBean tb1 = new TestBean();
+		tb1.setName("eva");
+		tb1.setAge(25);
+		TestBean tb2 = new TestBean();
+		tb2.setName("juergen");
+		tb2.setAge(99);
+		TestBean tb3 = new TestBean();
+		tb3.setName("Rod");
+		tb3.setAge(32);
+		List tbs = new ArrayList();
+		tbs.add(tb1);
+		tbs.add(tb2);
+		tbs.add(tb3);
+
+		Object[] sorted = BeanUtils.sortByProperty(tbs.toArray(), "name", true, false);
+		assertTrue("Correct 1. entry", sorted[0] == tb3);
+		assertTrue("Correct 2. entry", sorted[1] == tb1);
+		assertTrue("Correct 3. entry", sorted[2] == tb2);
+
+		sorted = BeanUtils.sortByProperty(tbs.toArray(), "name", false, false);
+		assertTrue("Correct 1. entry", sorted[0] == tb2);
+		assertTrue("Correct 2. entry", sorted[1] == tb1);
+		assertTrue("Correct 3. entry", sorted[2] == tb3);
+
+		sorted = BeanUtils.sortByProperty(tbs.toArray(), "name", true, true);
+		assertTrue("Correct 1. entry", sorted[0] == tb1);
+		assertTrue("Correct 2. entry", sorted[1] == tb2);
+		assertTrue("Correct 3. entry", sorted[2] == tb3);
+
+		sorted = BeanUtils.sortByProperty(tbs.toArray(), "age", false, false);
+		assertTrue("Correct 1. entry", sorted[0] == tb2);
+		assertTrue("Correct 2. entry", sorted[1] == tb3);
+		assertTrue("Correct 3. entry", sorted[2] == tb1);
+	}
+
+}
