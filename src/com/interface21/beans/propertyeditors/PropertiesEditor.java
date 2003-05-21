d783094854adf0865ab448111e5e0c144cadc9ae
Rod Johnson <rjohnson@interface21.com>
2003-05-21 17:02:37 +0000

    Now allows comments with leading whitespace: useful when using properties in XML bean definition files (the commonest usage)

--- src/com/interface21/beans/propertyeditors/PropertiesEditor.java
+++ src/com/interface21/beans/propertyeditors/PropertiesEditor.java
@@ -3,6 +3,9 @@ package com.interface21.beans.propertyeditors;
 import java.beans.PropertyEditorSupport;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 import java.util.StringTokenizer;
 
@@ -20,6 +23,13 @@ import java.util.StringTokenizer;
  * @version $Id$
  */
 public class PropertiesEditor extends PropertyEditorSupport {
+	
+	/**
+	 * Any of these characters, if they're first after whitespace
+	 * or first on a line, mean that the line is a comment and should
+	 * be ignored.
+	 */
+	private final static String COMMENT_MARKERS = "#!";
 	
 	/**
 	 * @see java.beans.PropertyEditor#setAsText(String)
@@ -76,6 +86,7 @@ public class PropertiesEditor extends PropertyEditorSupport {
 		Properties props = new Properties();
 		try {
 			props.load(new ByteArrayInputStream(s.getBytes()));
+			dropComments(props);
 		}
 		catch (IOException ex) {
 			// Shouldn't happen
@@ -83,6 +94,32 @@ public class PropertiesEditor extends PropertyEditorSupport {
 		}
 
 		return props;
+	}
+
+
+	/**
+	 * Remove comment lines. We shouldn't need to do this,
+	 * according to java.util.Properties documentation, but
+	 * if we don't we end up with properties like "#this=is a comment"
+	 * if we have whitespace before the comment marker.
+	 */
+	private void dropComments(Properties props) {
+		Iterator keys = props.keySet().iterator();
+		List commentKeys = new LinkedList();
+		while (keys.hasNext()) {
+			String key = (String) keys.next();
+			// A comment line starts with one of our comment markers
+			if (key.length() > 0 && COMMENT_MARKERS.indexOf(key.charAt(0)) != -1) {
+				// We can't actually remove it as we'll get a 
+				// concurrent modification exception with the iterator
+				commentKeys.add(key);
+			}
+		}
+		for (int i = 0; i < commentKeys.size(); i++) {
+			String key = (String) commentKeys.get(i);
+			System.out.println("Removed comment " + commentKeys.get(i));
+			props.remove(key);
+		}
 	}
 
 }
