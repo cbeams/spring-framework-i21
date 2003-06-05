70b3653cf1f2f2c262d2ce589c669d3129d03b6d
Rod Johnson <rjohnson@interface21.com>
2003-06-05 08:39:46 +0000

    First version

---
+++ src/com/interface21/beans/factory/support/ManagedMap.java
@@ -0,0 +1,19 @@
+/*
+ * The Spring Framework is published under the terms
+ * of the Apache Software License.
+ */
+ 
+package com.interface21.beans.factory.support;
+
+import java.util.HashMap;
+
+/**
+ * Tag subclass used to hold managed values, which may
+ * include runtime bean references
+ * @author Rod Johnson
+ * @since 27-May-2003
+ * @version $Id$
+ */
+public class ManagedMap extends HashMap {
+ 
+}
