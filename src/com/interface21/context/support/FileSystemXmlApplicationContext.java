77e70400c1d9521186e7d7badd4cae1eb1726879
Juergen Hoeller <jhoeller@interface21.com>
2003-06-18 18:43:32 +0000

    improved docs

--- src/com/interface21/context/support/FileSystemXmlApplicationContext.java
+++ src/com/interface21/context/support/FileSystemXmlApplicationContext.java
@@ -9,8 +9,8 @@ import com.interface21.util.StringUtils;
 
 /**
  * Standalone XML application context, taking the context definition
- * files from the file system. Mainly useful for test harnesses,
- * but also for standalone environments.
+ * files from the file system or from URLs. Mainly useful for test
+ * harnesses, but also for standalone environments.
  * @author Rod Johnson
  * @author Juergen Hoeller
  */
@@ -18,13 +18,28 @@ public class FileSystemXmlApplicationContext extends AbstractXmlApplicationConte
 
 	private String configLocation;
 
-	public FileSystemXmlApplicationContext(String locations) throws ApplicationContextException, IOException {
+	/**
+	 * Create a new FileSystemXmlApplicationContext with the given location.
+	 * The locations specify the parents in descending order, i.e. the last
+	 * location is the one of this context.
+	 * @param locations comma-delimited String consisting of locations
+	 */
+	public FileSystemXmlApplicationContext(String locations)
+	    throws ApplicationContextException, IOException {
 		this(StringUtils.commaDelimitedListToStringArray(locations));
 	}
 
-	public FileSystemXmlApplicationContext(String[] locations) throws ApplicationContextException, IOException {
-		if (locations.length == 0)
-			throw new ApplicationContextException("At least 1 configLocation required");
+	/**
+	 * Create a new FileSystemXmlApplicationContext with the given locations.
+	 * The locations specify the parents in descending order, i.e. the last
+	 * location is the one of this context.
+	 * @param locations String array consisting of locations
+	 */
+	public FileSystemXmlApplicationContext(String[] locations)
+	    throws ApplicationContextException, IOException {
+		if (locations.length == 0) {
+			throw new ApplicationContextException("At least 1 config location required");
+		}
 
 		this.configLocation = locations[locations.length - 1];
 		logger.debug("Trying to open XML application context file '" + this.configLocation + "'");
@@ -34,11 +49,15 @@ public class FileSystemXmlApplicationContext extends AbstractXmlApplicationConte
 			// There were parent(s)
 			String[] parentLocations = new String[locations.length - 1];
 			System.arraycopy(locations, 0, parentLocations, 0, locations.length - 1);
-			logger.debug("Setting parent context for locations: [" + StringUtils.arrayToDelimitedString(parentLocations, ","));
+			if (logger.isDebugEnabled()) {
+				logger.debug("Setting parent context for locations: [" +
+										 StringUtils.arrayToDelimitedString(parentLocations, ","));
+			}
 			ApplicationContext parent = createParentContext(parentLocations);
 			setParent(parent);
 		}
 
+		// initialize this context
 		refresh();
 	}
 	
