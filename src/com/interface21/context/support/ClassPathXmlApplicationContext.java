81efe85f0b9e01c446e6b467c60b577efa3d358e
Juergen Hoeller <jhoeller@interface21.com>
2003-06-24 12:23:58 +0000

    fixed createParentContext, added docs

--- src/com/interface21/context/support/ClassPathXmlApplicationContext.java
+++ src/com/interface21/context/support/ClassPathXmlApplicationContext.java
@@ -4,13 +4,18 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import com.interface21.context.ApplicationContextException;
+import com.interface21.context.ApplicationContext;
 
 /**
  * Standalone XML application context, taking the context definition
  * files from the classpath. Mainly useful for test harnesses,
  * but also for application contexts embedded within JARs.
+ *
  * <p>Note: Generally treats (file) paths as class path resources,
  * when using ApplicationContext.getResourceAsStream.
+ * Only supports full classpath names including package specification,
+ * like "/mypackage/myresource.dat".
+ *
  * @author Rod Johnson
  * @author Juergen Hoeller
  * @see com.interface21.context.ApplicationContext#getResourceAsStream
@@ -26,8 +31,14 @@ public class ClassPathXmlApplicationContext extends FileSystemXmlApplicationCont
 		super(locations);
 	}
 
+	protected ApplicationContext createParentContext(String[] locations) throws IOException {
+		return new ClassPathXmlApplicationContext(locations);
+	}
+
 	/**
-	 * This implementation treats paths as class path resources.
+	 * This implementation treats paths as classpath resources.
+	 * Only supports full classpath names including package specification,
+	 * like "/mypackage/myresource.dat".
 	 */
 	protected InputStream getResourceByPath(String path) throws IOException {
 		if (!path.startsWith("/")) {
