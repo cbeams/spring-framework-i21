eb950f5eb770ede6821b5a9a2f699be87ac15e08
Rod Johnson <rjohnson@interface21.com>
2003-07-05 16:27:45 +0000

    Now has only one constructor, which requires message has well as bean name

--- src/com/interface21/beans/factory/NoSuchBeanDefinitionException.java
+++ src/com/interface21/beans/factory/NoSuchBeanDefinitionException.java
@@ -7,21 +7,13 @@ import com.interface21.beans.BeansException;
  * Exception thrown when a BeanFactory is asked for a bean 
  * instance name for which it cannot find a definition.
  * @author Rod Johnson
+ * @version $Id$
  */
 public class NoSuchBeanDefinitionException extends BeansException {
 	
 	/** Name of the missing bean */
-	private String name;
+	private final String name;
 
-    /**
-     * Creates new <code>NoSuchBeanDefinitionException</code>..
-     * @param name the name of the missing bean
-     */
-    public NoSuchBeanDefinitionException(String name) {
-        super("No bean named [" + name + "] is defined", null);
-        this.name = name;
-    }
-	
 	 /**
      * Creates new <code>NoSuchBeanDefinitionException</code>..
      * @param name the name of the missing bean
