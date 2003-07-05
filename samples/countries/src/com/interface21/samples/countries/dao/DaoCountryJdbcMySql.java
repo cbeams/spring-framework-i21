313c37488553e80896b057990e96289e90e1e6b1
Jean-Pierre Pawlak <pawlakjp@users.sourceforge.net>
2003-07-05 14:50:32 +0000

    *** empty log message ***

--- samples/countries/src/com/interface21/samples/countries/dao/DaoCountryJdbcMySql.java
+++ samples/countries/src/com/interface21/samples/countries/dao/DaoCountryJdbcMySql.java
@@ -1,16 +1,21 @@
 package com.interface21.samples.countries.dao;
 
+import com.interface21.jdbc.core.JdbcTemplate;
+
 /**
  * @author Jean-Pierre Pawlak
  */
 public class DaoCountryJdbcMySql extends AbstractDaoCountryJdbc {
 
 	/**
+	 * The table could define lang + code as primary key and innoDB type.
+	 * <br>Index may also be created, but for a so small table, the interest is not clear.
 	 * @see com.interface21.samples.countries.dao.IDaoCountry#initBase()
 	 */
 	public void initBase() {
-		// TODO Auto-generated method stub
-
+		JdbcTemplate tpl = new JdbcTemplate();
+		tpl.setDataSource(this.getDataSource());
+		tpl.update("DROP TABLE IF EXISTS countries");
+		tpl.update("CREATE TABLE countries (lang CHAR(2), code CHAR(2), name VARCHAR(50)");
 	}
-
 }
