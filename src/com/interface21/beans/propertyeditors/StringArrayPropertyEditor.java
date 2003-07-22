dd9398df52572b137633c65e8ac7612a77d93f73
Juergen Hoeller <jhoeller@interface21.com>
2003-07-22 18:36:30 +0000

    added getAsText implementation

--- src/com/interface21/beans/propertyeditors/StringArrayPropertyEditor.java
+++ src/com/interface21/beans/propertyeditors/StringArrayPropertyEditor.java
@@ -5,20 +5,21 @@ import java.beans.PropertyEditorSupport;
 import com.interface21.util.StringUtils;
 
 /**
- * Properties editor for String[] type.
- * Strings must be in CSV format.
- * This property editor is registered by BeanWrapperImpl.
+ * Properties editor for String[] type. Strings must be in CSV format.
+ * This property editor is automatically registered by BeanWrapperImpl.
  * @author Rod Johnson
+ * @see com.interface21.beans.BeanWrapperImpl
  */
 public class StringArrayPropertyEditor extends PropertyEditorSupport {
-	
-	/**
-	 * @see java.beans.PropertyEditor#setAsText(String)
-	 */
+
 	public void setAsText(String s) throws IllegalArgumentException {
 		String[] sa = StringUtils.commaDelimitedListToStringArray(s);
 		setValue(sa);
 	}
 
-}
+	public String getAsText() {
+		String[] array = (String[]) this.getValue();
+		return StringUtils.arrayToCommaDelimitedString(array);
+	}
 
+}
