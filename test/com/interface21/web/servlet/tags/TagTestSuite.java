f937bccfe813dbd17cba18f715ba5e86d77f10e9
Alef Arendsen <alef@interface21.com>
2003-07-24 08:29:02 +0000

    * Added testing of property exposing method of BindTag

--- test/com/interface21/web/servlet/tags/TagTestSuite.java
+++ test/com/interface21/web/servlet/tags/TagTestSuite.java
@@ -206,6 +206,31 @@ public class TagTestSuite extends TestCase {
 		assertTrue("Correct errorMessagesAsString", "message &amp; 1 - message2".equals(status.getErrorMessagesAsString(" - ")));
 	}
 
+	public void testPropertyExposing() throws JspException {
+		MockPageContext pc = createPageContext();
+		TestBean tb = new TestBean();
+		tb.setName("name1");
+		BindException errors = new BindException(tb, "tb");
+		errors.rejectValue("name", "code1", null, "message & 1");
+		errors.rejectValue("name", "code2", null, "message2");
+		pc.getRequest().setAttribute(BindException.ERROR_KEY_PREFIX + "tb", errors);
+
+		// test global property (should be null)
+		BindTag tag = new BindTag();
+		tag.setPageContext(pc);
+		tag.setPath("tb");
+		assertTrue("Correct doStartTag return value", tag.doStartTag() == Tag.EVAL_BODY_INCLUDE);
+		assertNull(tag.getProperty());
+
+		// test property set (tb.name)
+		tag.release();
+		tag.setPageContext(pc);
+		tag.setPath("tb.name");
+		assertTrue("Correct doStartTag return value", tag.doStartTag() == Tag.EVAL_BODY_INCLUDE);
+		assertEquals("name", tag.getProperty());
+	}
+
+
 	public void testBindTagWithoutBean() throws JspException {
 		MockPageContext pc = createPageContext();
 		BindTag tag = new BindTag();
