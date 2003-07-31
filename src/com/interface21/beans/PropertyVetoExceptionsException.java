1d1c6fd8a4d04219de915cca276e48ecfea8d22e
Juergen Hoeller <jhoeller@interface21.com>
2003-07-31 18:46:17 +0000

    print stacktraces of root causes

--- src/com/interface21/beans/PropertyVetoExceptionsException.java
+++ src/com/interface21/beans/PropertyVetoExceptionsException.java
@@ -1,4 +1,3 @@
-
 package com.interface21.beans;
 
 import java.beans.PropertyVetoException;
@@ -8,73 +7,69 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 
 import com.interface21.util.StringUtils;
 
 /**
  * Combined exception, composed of individual binding exceptions.
- * An object of this class is created at the beginning of the binding process,
- * and errors added to it as necessary.
- * The binding process continues when it encounters application-level
+ * An object of this class is created at the beginning of the binding
+ * process, and errors added to it as necessary.
+ *
+ * <p>The binding process continues when it encounters application-level
  * exceptions, applying those changes that can be applied and storing
  * rejected changes in an object of this class.
- * @author  Rod Johnson
+ *
+ * @author Rod Johnson
  * @since 18 April 2001
  * @version $Id$
  */
 public class PropertyVetoExceptionsException extends BeansException {
 
-    //---------------------------------------------------------------------
-	// Instance data
-    //---------------------------------------------------------------------
 	/** List of ErrorCodedPropertyVetoException objects */
-    private List    exceptions = new LinkedList();
-    
+	private List exceptions = new LinkedList();
+
 	/** BeanWrapper wrapping the target object for binding */
-    private BeanWrapper beanWrapper;
+	private BeanWrapper beanWrapper;
 
-    //---------------------------------------------------------------------
-    // Constructors
-    //---------------------------------------------------------------------
-    /** 
-     * Creates new empty PropertyVetoExceptionsException
-     * We'll add errors to it as we attempt to bind properties.
-     */
-    PropertyVetoExceptionsException(BeanWrapper beanWrapper) {
-        super("PropertyVetoExceptionsException", null);
-        this.beanWrapper = beanWrapper;
-    }
+	/**
+	 * Creates new empty PropertyVetoExceptionsException
+	 * We'll add errors to it as we attempt to bind properties.
+	 */
+	PropertyVetoExceptionsException(BeanWrapper beanWrapper) {
+		super("PropertyVetoExceptionsException", null);
+		this.beanWrapper = beanWrapper;
+	}
 
+	/**
+	 * Return the BeanWrapper that generated this exception.
+	 */
+	public BeanWrapper getBeanWrapper() {
+		return beanWrapper;
+	}
 
-    //---------------------------------------------------------------------
-	// Public methods
-    //---------------------------------------------------------------------
-    /**
-     * Return the BeanWrapper that generated this exception
-     * @return the BeanWrapper that generated this exception
-     */
-    public BeanWrapper getBeanWrapper() {
-        return beanWrapper;
-    }
-	
-	/** Return the object we're binding to
+	/**
+	 * Return the object we're binding to.
 	 */
 	public Object getBindObject() {
-        return beanWrapper.getWrappedInstance();
-    }
-	
-	/** Messages keyed by field name */
-	public Map getMessages() {
-		HashMap m = new HashMap();
-		for (int i = 0; i < exceptions.size(); i++) {
-			ErrorCodedPropertyVetoException ex = (ErrorCodedPropertyVetoException) exceptions.get(i);
-			m.put(ex.getPropertyChangeEvent().getPropertyName(), ex.getErrorCode() + ": " + ex.getMessage());
-		}
-		return m;
+		return beanWrapper.getWrappedInstance();
 	}
-	
-	/** 
-	 * Errors iterator 
+
+	/**
+	 * Return messages keyed by field name.
+	 */
+	public Map getMessages() {
+		Map msgs = new HashMap();
+		for (Iterator it = exceptions.iterator(); it.hasNext();) {
+			ErrorCodedPropertyVetoException ex = (ErrorCodedPropertyVetoException) it.next();
+			msgs.put(ex.getPropertyChangeEvent().getPropertyName(), ex.getErrorCode() + ": " + ex.getMessage());
+		}
+		return msgs;
+	}
+
+	/**
+	 * Return an errors iterator.
 	 */
 	public Iterator iterator() {
 		return new Iterator() {
@@ -91,72 +86,88 @@ public class PropertyVetoExceptionsException extends BeansException {
 				throw new UnsupportedOperationException("Cannot remove elements from PropertyVetoExceptionExceptions iterator");
 			}
 		};
-	}	// iterator
-    
-	/** 
-	 * If this returns 0, no errors were encountered
-	 * during binding 
-	 */
-    public int getExceptionCount() {
-        return exceptions.size();
-    }
-    
+	}
+
 	/**
-	 * @return an array of the exceptions stored in this object.
-	 * Will return the empty array (not null) if there were no errors. 
+	 * If this returns 0, no errors were encountered during binding.
 	 */
-    public ErrorCodedPropertyVetoException[] getPropertyVetoExceptions() {
-        return (ErrorCodedPropertyVetoException[]) exceptions.toArray(new ErrorCodedPropertyVetoException[0]);
-    }
-    
-    /** 
-	 * @return the exception for this field, or null if there isn't one 
-     */
-    public ErrorCodedPropertyVetoException getPropertyVetoException(String propertyName) {
-        for (Iterator itr = exceptions.iterator(); itr.hasNext(); ) {
-            ErrorCodedPropertyVetoException pve = (ErrorCodedPropertyVetoException) itr.next();
-            if (propertyName.equals(pve.getPropertyChangeEvent().getPropertyName()))
-                return pve;
-        }
-        return null;
-    }
-    
+	public int getExceptionCount() {
+		return exceptions.size();
+	}
+
+	/**
+	 * Return an array of the exceptions stored in this object.
+	 * Will return the empty array (not null) if there were no errors.
+	 */
+	public ErrorCodedPropertyVetoException[] getPropertyVetoExceptions() {
+		return (ErrorCodedPropertyVetoException[]) exceptions.toArray(new ErrorCodedPropertyVetoException[0]);
+	}
+
+	/**
+	 * Return the exception for this field, or null if there isn't one.
+	 */
+	public ErrorCodedPropertyVetoException getPropertyVetoException(String propertyName) {
+		for (Iterator itr = exceptions.iterator(); itr.hasNext();) {
+			ErrorCodedPropertyVetoException pve = (ErrorCodedPropertyVetoException) itr.next();
+			if (propertyName.equals(pve.getPropertyChangeEvent().getPropertyName()))
+				return pve;
+		}
+		return null;
+	}
+
+
 	//---------------------------------------------------------------------
 	// Package methods allowing errors to be added
-    //---------------------------------------------------------------------
-    void addPropertyVetoException(PropertyVetoException ex) {
+	//---------------------------------------------------------------------
+
+	void addPropertyVetoException(PropertyVetoException ex) {
 		if (ex instanceof ErrorCodedPropertyVetoException)
 			addErrorCodedPropertyVetoException((ErrorCodedPropertyVetoException) ex);
 		else
-        	exceptions.add(new ErrorCodedPropertyVetoException(ex));
-    }
-	
+			exceptions.add(new ErrorCodedPropertyVetoException(ex));
+	}
+
 	void addErrorCodedPropertyVetoException(ErrorCodedPropertyVetoException ex) {
-        exceptions.add(ex);
-    }
-	
+		exceptions.add(ex);
+	}
+
 	void addTypeMismatchException(TypeMismatchException ex) {
 		// Need proper stack trace!?
-        addPropertyVetoException(new ErrorCodedPropertyVetoException(ex));
-    }
-	
+		addPropertyVetoException(new ErrorCodedPropertyVetoException(ex));
+	}
+
 	void addMissingFields(InvalidPropertyValuesException ex) {
 		// Need proper stack trace!?
 		for (int i = 0; i < ex.getMissingFields().size(); i++) {
 			addPropertyVetoException(new ErrorCodedPropertyVetoException(getBindObject(), (InvalidPropertyValuesException.MissingFieldException) ex.getMissingFields().get(i)));
 		}
-    }
-	
+	}
+
 	void addMethodInvocationException(MethodInvocationException ex) {
 		// Need proper stack trace!?
-        addPropertyVetoException(new ErrorCodedPropertyVetoException(ex));
-    }
-    
-    public String toString() {
-        String s = "PropertyVetoExceptionsException: " + getExceptionCount() + " errors:-- ";
-		// Stack traces for each!?
-        s += StringUtils.collectionToDelimitedString(exceptions, ";");
-        return s;
-    }
+		addPropertyVetoException(new ErrorCodedPropertyVetoException(ex));
+	}
 
-}	// class PropertyVetoExceptionsException
+	public void printStackTrace(PrintStream ps) {
+		for (Iterator it = exceptions.iterator(); it.hasNext();) {
+			ps.println(this);
+			ErrorCodedPropertyVetoException ex = (ErrorCodedPropertyVetoException) it.next();
+			ex.getRootCause().printStackTrace(ps);
+		}
+	}
+
+	public void printStackTrace(PrintWriter pw) {
+		for (Iterator it = exceptions.iterator(); it.hasNext();) {
+			pw.println(this);
+			ErrorCodedPropertyVetoException ex = (ErrorCodedPropertyVetoException) it.next();
+			ex.getRootCause().printStackTrace(pw);
+		}
+	}
+
+	public String toString() {
+		String s = "PropertyVetoExceptionsException: " + getExceptionCount() + " errors:-- ";
+		s += StringUtils.collectionToDelimitedString(exceptions, ";");
+		return s;
+	}
+
+}
