bd5bf3885d67fbb0913430e84e4fe4b07ef1172f
Juergen Hoeller <jhoeller@interface21.com>
2003-07-31 18:46:58 +0000

    polishing

--- src/com/interface21/beans/BeansException.java
+++ src/com/interface21/beans/BeansException.java
@@ -1,4 +1,3 @@
-
 package com.interface21.beans;
 
 import com.interface21.core.NestedRuntimeException;
@@ -8,29 +7,29 @@ import com.interface21.core.NestedRuntimeException;
  * and subpackages. Note that this is a runtime (unchecked) exception.
  * Beans exceptions are usually fatal; there is no reason for them to be
  * checked.
- * @author  Rod Johnson
+ * @author Rod Johnson
  */
 public abstract class BeansException extends NestedRuntimeException {
 
-    /**
-     * Constructs an <code>BeansException</code> with the specified message
-     * and root cause.
-     * @param msg the detail message.
-     * @param t the root cause
-     */
-    public BeansException(String msg, Throwable t) {
-        super(msg, t);
-    }
-    
-    /**
-     * Constructs an <code>BeansException</code> with the specified message
-     * and no root cause.
-     * @param msg the detail message.
-     */
-    public BeansException(String msg) {
-        super(msg);
-    }
-    
+	/**
+	 * Constructs an <code>BeansException</code> with the specified message
+	 * and root cause.
+	 * @param msg the detail message.
+	 * @param t the root cause
+	 */
+	public BeansException(String msg, Throwable t) {
+		super(msg, t);
+	}
+
+	/**
+	 * Constructs an <code>BeansException</code> with the specified message
+	 * and no root cause.
+	 * @param msg the detail message.
+	 */
+	public BeansException(String msg) {
+		super(msg);
+	}
+
 }
 
 
